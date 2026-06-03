8ed924aeb0e382c3e8b1c377ca6edeb890758dcd
Evonnezl <zl1709351471@outlook.com>
2026-06-03 20:56:14 +0800

    完成前端用户接口调用测试

---
+++ frontend/src/views/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>用户列表</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.role }} - {{ user.phone }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    fetch('http://localhostz:8080/users')
+      .then(res => res.json())
+      .then(data => this.users = data)
+      .catch(err => console.error(err));
+  }
+}
+</script>
